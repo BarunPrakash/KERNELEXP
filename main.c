c23ca118dd676144b5c48804f578c1619d6e1307
BarunPrakash <brnprksh@gmail.com>
2021-08-13 18:00:49 +0530

    sub is added and main is midified!

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 
 int add(int x ,int y);
+int sub(int x ,int y);
 int main()
 {
 	int a,b;
 	printf("Enter the value a and b!");
 	scanf("%d %d",&a ,&b);
 	printf("This is barun\n result:%d",add(a,b));
+	printf("This is barun\n result:%d",sub(a,b));
 	return 0;
 }
